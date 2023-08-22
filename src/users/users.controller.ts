e61a61e7b9e39edc3e93d57686d3a00bd3f33a9a
Muhamad Hamdanil <hamdanilm96@gmail.com>
2023-08-22 15:46:54 +0800

    create user module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from 'src/dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  async register(@Body() createUserDTO: CreateUserDTO) {
+    return this.usersService.createUser(createUserDTO);
+  }
+}
+
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDTO } from 'src/dto/create-user.dto';
+import { PrismaService } from '../services/prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) { }
+
+  // Find all users
+  async findAllUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  // Find user by email
+  async findUserByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { email } })
+  }
+
+  // Find user by uuid
+  async findUserByUUID(uuid: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { uuid } })
+  }
+
+  async createUser(data: { email: string, password: string, name?: string }): Promise<User> {
+    const existingUser = await this.prisma.user.findUnique({
+        where: {
+            email: data.email
+        }
+    })
+
+    if (existingUser) {
+        throw new ConflictException('Email alrady in use')
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    return this.prisma.user.create({
+        data: {
+            email: data.email,
+            password: data.password,
+            name: data.name,
+        }
+    })
+  }
+}
